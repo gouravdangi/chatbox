778a236bad03c1687c982a08573ad26f16cb798d
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-07-29 18:27:24 +0530

    basic chat box done

--- chat.js
+++ chat.js
@@ -12,14 +12,6 @@ document.getElementById("chatbot_toggle").onclick = function () {
     }
 }
 
-// document.querySelector("#message").addEventListener("keypress", async (e) => {
-//     if (e.key == "Enter") {
-//         document.querySelector("#message").value = "";
-//          console.log("hi");
-//     }
-// })
-
-
 
 function getTime() {
     let today = new Date();
