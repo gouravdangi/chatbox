adc52d49f5cab653efcf07d406e1a970350d15cc
gouravdangi <63147754+gouravdangi@users.noreply.github.com>
2021-07-29 12:10:48 +0530

    base-js (styling)

---
+++ chat.js
@@ -0,0 +1,13 @@
+document.getElementById("chatbot_toggle").onclick = function () {
+    if (document.getElementById("chatbot").classList.contains("collapsed")) {
+        document.getElementById("chatbot").classList.remove("collapsed")
+        document.getElementById("chatbot_toggle").children[0].style.display = "none"
+        document.getElementById("chatbot_toggle").children[1].style.display = ""
+        setTimeout(addResponseMsg, 1000, "Hi")
+    }
+    else {
+        document.getElementById("chatbot").classList.add("collapsed")
+        document.getElementById("chatbot_toggle").children[0].style.display = ""
+        document.getElementById("chatbot_toggle").children[1].style.display = "none"
+    }
+}
