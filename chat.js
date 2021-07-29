03a316c075156a382d99ca1a4f8b8fe60123d51a
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-07-29 14:59:09 +0530

    basic ui done

--- chat.js
+++ chat.js
@@ -9,3 +9,10 @@ document.getElementById("chatbot_toggle").onclick = function () {
         document.getElementById("chatbot").classList.add("collapsed");
     }
 }
+
+document.querySelector("#message").addEventListener("keypress", async (e) => {
+    if (e.key == "Enter") {
+        document.querySelector("#message").value = "";
+         console.log("hi");
+    }
+})
