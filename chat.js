b1c20fc086f5938dee2f6678bb04bb52f90d1ae2
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2021-07-29 17:51:02 +0530

    adding js

--- chat.js
+++ chat.js
@@ -1,3 +1,5 @@
+window.onload = ()=>{
+    
 document.getElementById("chatbot_toggle").onclick = function () {
     if (document.getElementById("chatbot").classList.contains("collapsed")) {
         document.getElementById("chatbot").classList.remove("collapsed")
@@ -10,9 +12,63 @@ document.getElementById("chatbot_toggle").onclick = function () {
     }
 }
 
-document.querySelector("#message").addEventListener("keypress", async (e) => {
-    if (e.key == "Enter") {
-        document.querySelector("#message").value = "";
-         console.log("hi");
+// document.querySelector("#message").addEventListener("keypress", async (e) => {
+//     if (e.key == "Enter") {
+//         document.querySelector("#message").value = "";
+//          console.log("hi");
+//     }
+// })
+
+
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+
+function firstBotMessage() {
+    let firstMessage = "Hi, How can I help you?"
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    // document.getElementById("userInput").scrollIntoView(false);
+}
+
+
+firstBotMessage();
+
+
+let getResponse = ()=>{
+    let userText = $("#message").val();
+    console.log(userText);
+    let userHtml = `<p class = 'userText'><span>${userText}</span></p>`
+    console.log(userHtml);
+    $("#message").val("");
+    $("#message-box").append(userHtml);
+}
+
+$("#message").keypress(async(e)=>{
+    if(e.key == "Enter"){
+        console.log('pressed enter');
+        getResponse();
     }
 })
+
+}
+
+
